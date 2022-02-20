b73da9676edee83a317e8e7b266e9ef5f98ba945
Sayed Saddam Hussain <48250591+sayedsaddam@users.noreply.github.com>
2022-02-20 13:21:35 +0500

    updated 3 files, fixed the indentation on the about page.

--- application/views/admin/articles.php
+++ application/views/admin/articles.php
@@ -1,7 +1,29 @@
 <div class="container">
 	<div class="row">
-		<div class="col-md-6">
-			
+		<div class="col-md-12">
+			<table class="table table-condensed table-hover table-sm">
+				<thead>
+					<tr>
+						<th>ID</th>
+						<th>Title</th>
+						<th>Slug</th>
+						<th>Author</th>
+						<th>Action</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>1</td>
+						<td>Test Article</td>
+						<td>test-article</td>
+						<td>Admin</td>
+						<td>
+							<a href="" class="btn btn-primary btn-sm">edit</a>
+							<a href="" class="btn btn-danger btn-sm">delete</a>
+						</td>
+					</tr>
+				</tbody>
+			</table>
 		</div>
 	</div>
 </div>
