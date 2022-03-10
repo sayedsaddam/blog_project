2bebba24eef6796e2e46f25643409ff09ac8cda7
Sayed Saddam Hussain <48250591+sayedsaddam@users.noreply.github.com>
2022-03-11 00:22:53 +0500

    modified 2 files inlcuding component and a view file. added more sections on to the home page, also, added footer on the site.

--- application/views/components/foot.php
+++ application/views/components/foot.php
@@ -1,9 +1,29 @@
-
-<div class="container-fluid mt-5 pt-5 mb-3">
-	<div class="row">
-		<div class="col-md-12 text-right">
-			<hr>
-			<small>&copy; <?php echo date('Y'); ?> - <?php echo 'AgroLabs'; ?></small>
+<div class="jumbotron bg-dark text-light mb-0 mt-5">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4">
+				<h5>About</h5>
+				<p class="font-weight-light">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum quo ipsa officiis laudantium porro assumenda accusamus reiciendis, omnis laboriosam consequatur sed sint, hic laborum beatae, expedita esse unde. Omnis, porro!</p>
+			</div>
+			<div class="col-md-4">
+				<h5>Quick Links</h5>
+				<li class="mb-0 font-weight-light"><a class="text-light" href="#">Home</a></li>
+				<li class="mb-0 font-weight-light"><a class="text-light" href="#">About</a></li>
+				<li class="mb-0 font-weight-light"><a class="text-light" href="#">Contact</a></li>
+				<li class="mb-0 font-weight-light"><a class="text-light" href="#">Careers</a></li>
+				<li class="mb-0 font-weight-light"><a class="text-light" href="#">Blog</a></li>
+			</div>
+			<div class="col-md-4">
+				<h5>Find Us <small class="font-weight-light">All over the place</small></h5>
+				<p class="font-weight-light">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis deserunt culpa distinctio perspiciatis fugit nam voluptatem fugiat corporis ipsam est ea minus magnam sed hic tenetur ipsa accusantium, quas eligendi.</p>
+			</div>
+		</div>
+	</div>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-12 text-right">
+				<p class="font-weight-light">&copy; Copyright <?= date('Y'); ?> AgroLabs</p>
+			</div>
 		</div>
 	</div>
 </div>
