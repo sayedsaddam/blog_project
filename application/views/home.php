0e75582911213b1fad5032c28267118f8bfd7576
Sayed Saddam Hussain <48250591+sayedsaddam@users.noreply.github.com>
2022-03-12 00:29:42 +0500

    updated home.php, added an hr tag to separate a section.

--- application/views/home.php
+++ application/views/home.php
@@ -92,6 +92,7 @@
 			</div>
 		</div>
 	</div>
+	<hr>
 	<div class="row">
 		<div class="col-md-10 offset-md-1">
 			<h1 class="font-weight-bold">New Arrivals <small class="text-muted font-weight-light">Stay up-to-date about new products.</small></h1>
