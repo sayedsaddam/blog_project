f7d2bb2ed4e11422d826e5eab7ca4c73ff355d81
Sayed Saddam Hussain <48250591+sayedsaddam@users.noreply.github.com>
2022-04-21 02:01:58 +0500

    updated losers.php, added content on to it.

--- application/views/losers.php
+++ application/views/losers.php
@@ -1,35 +1,56 @@
 <div class="container">
-	<div class="row mb-4">
+	<div class="row mb-3">
 		<div class="col-12">
 			<h1 class="h1 font-weight-bold">Loser of the Decade</h1>
 		</div>
 	</div>
 	<div class="row">
-		<div class="col-md-5">
+		<div class="col-md-4 col-sm-12 mt-2">
 			<figure class="figure">
 				<img src="<?= base_url('assets/images/loser-ss.jpeg'); ?>" class="img-responsive img-fluid mb-3" alt="loser-of-the-day">
 				<figcaption class="figure-caption">
 					<p class="lead">Mr. Shehbaz Sharif, Lahore-PK <br>Political Beggar of Pakistan</p>
 			</figure>
 		</div>
-		<div class="col-md-7">
-			<h2>History [ Begging ]</h2>
-			<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae amet nostrum sequi reprehenderit dolore quisquam blanditiis natus dignissimos veritatis animi porro, fugiat hic molestiae explicabo illum deserunt ea nesciunt ducimus!</p>
-
+		<div class="col-md-8 col-sm-12">
+			<p class="text-justify"><span class="font-weight-bold lead">History:</span> Mian Muhammad Shehbaz Sharif (Punjabi and Urdu: میاں محمد شہباز شریف born on 23 September 1951) is a Pakistani politician serving as the 23rd and current prime minister of Pakistan, in office since 11 April 2022 [Illegally] 85% of the Pakistanis do not consider him as their Prime Minister. Rest of the 20% badly need education.</p>
+			<p class="text-justify">He is the current president of the Pakistan Muslim League (N) (PML-N). Previously in his political career, he served as the Chief Minister of Punjab three times, making him the longest-serving Chief Minister of Punjab</p>
+			<p class="text-justify">He has two brothers, Abbas Sharif and Nawaz Sharif. Nawaz is a three-time elected Prime Minister of Pakistan. His sister-in-law, Kulsoom Nawaz Sharif, was the First Lady of Pakistan for three non-consecutive terms.</p>
+			<p class="text-justify">Shehbaz is a businessman by profession and jointly owns Ittefaq Group, a multimillion-dollar steel conglomerate. In 2013, it was noted that Shehbaz is wealthier than his elder brother Nawaz with ₨336,900,000 (US$2.1 million. Shehbaz was elected to the Provincial Assembly of the Punjab in 1988 and to the National Assembly of Pakistan in 1990. He was again elected to the Punjab Assembly in 1993 and named Leader of the Opposition.</p>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12 col-sm-12">
 			<h2>Background</h2>
-			<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint voluptatem harum, iste odio molestiae similique eveniet amet! Beatae recusandae praesentium modi reprehenderit obcaecati, nobis, accusamus molestias voluptatem quis quia vel?</p>
+			<p class="text-justify">He was elected as chief minister of Pakistan's most populous province, Punjab, for the first time on 20 February 1997. After the 1999 Pakistani coup d'état, Shehbaz along with his family spent years of self-exile in Saudi Arabia, returning to Pakistan in 2007. Shehbaz was appointed Chief Minister for a second term after the PML-N's victory in Punjab province in the 2008 Pakistani general election. He was elected as Chief Minister of Punjab for the third time in the 2013 general election and served his term until his party's defeat in the 2018 general election. During his tenure as chief minister, Shehbaz enjoyed a reputation as a highly competent and diligent administrator.[3] He initiated ambitious infrastructure projects in Punjab and was noted for his efficient governance. Shehbaz was nominated as the president of the Pakistan Muslim League-N after his brother, Nawaz Sharif, was disqualified from holding office in the wake of the Panama Papers case. He was nominated as the Leader of the Opposition after the 2018 election.</p>
 
-			<h2>Before joining Politics</h2>
-			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam iure praesentium eius. Ullam doloremque iste aspernatur? Tempora nisi dicta provident, deleniti, voluptatum repellendus sunt necessitatibus repellat itaque cum eius dignissimos!</p>
+			<p class="text-justify">In December 2019, the National Accountability Bureau (NAB) froze 23 properties belonging to Shehbaz and his son, Hamza Sharif, accusing them of money laundering. On 28 September 2020, NAB arrested Shehbaz at Lahore High Court and indicted him on charges of money laundering. He was incarcerated pending trial. On 14 April 2021, Lahore High Court released him on bail in money laundering reference. Amid the 2020–2022 Pakistani political crises, he was elected Prime Minister on 11 April 2022 after the no-confidence motion against Imran Khan.</p>
 
-			<h2>After joining Politics</h2>
-			<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio libero facere, est sequi sint recusandae tempore quidem aliquam animi nisi nulla voluptatum consequuntur, assumenda accusamus voluptates repudiandae deserunt at aut?</p>
+			<h2>Initial Political Career</h2>
+			<p class="text-justify">Shehbaz began his political career after getting elected to the Provincial Assembly of Punjab from Constituency PP-122 (Lahore-VII) as a candidate of Islami Jamhoori Ittehad (JI) in 1988 general election. He secured 22,372 votes and defeated a candidate of Pakistan Peoples Party (PPP). However his term prematurely ended in 1990 when the assemblies were dissolved.</p>
 
-			<h2>How did he become a PM?</h2>
-			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam officia praesentium nulla delectus accusantium incidunt, earum facilis officiis iusto sed, optio nostrum laudantium, aliquam ipsum modi labore. Sunt, officiis aliquid!</p>
+			<p class="text-justify">He was re-elected to the Provincial Assembly of Punjab from Constituency PP-124 (Lahore-IX) as a candidate of IJI in 1990 general election. He received 26,408 votes and defeated a candidate of Pakistan Democratic Alliance (PDA). In the same election, he was elected to the National Assembly of Pakistan from Constituency NA-96 (Lahore-V) as a candidate of IJI. He secured 54,506 votes and defeated Jehangir Bader. He vacated the Provincial Punjab Assembly seat to retain his National Assembly seat. His term prematurely ended in 1993 when the assemblies were dissolved.</p>
 
-			<h2>The Illegal and disgraced PM ever</h2>
-			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, nobis harum nulla saepe labore deserunt obcaecati nisi! Ipsam, dolore nisi eum quod consequuntur molestiae et suscipit nihil asperiores eos eligendi.</p>
+			<p class="text-justify">He was re-elected to the Provincial Assembly of Punjab from Constituency PP-125 (Lahore-X) as a candidate of Pakistan Muslim League (N) (PML-N) in 1993 general election. He received 28,068 votes and defeated a candidate of PPP. In the same election, he was re-elected to the National Assembly from Constituency NA-96 (Lahore-V) as a candidate of PML-N. He secured 55,867 votes and defeated Yousuf Salahuddin. He vacated the National Assembly seat and retained his Provincial Punjab Assembly seat. Shortly after the election, he was elected Leader of Opposition in the Provincial Assembly of Punjab. During his tenure as leader of the opposition, he remained in the United Kingdom for some years due for medical treatment. In his absence, Chaudhry Pervaiz Elahi was made acting leader of the opposition in the Punjab Assembly. His term as Member of the Punjab Assembly and the Leader of the Opposition prematurely ended in November 1996 when the assemblies were dissolved.</p>
+
+			<h2>After joining Politics - Crime history begins</h2>
+			<p class="lead">Sabzazar Case - The first case</p>
+			<p class="text-justify">In 1999, a complainant Saeeduddin lodged FIR and accused Shehbaz of allowing the Sabzazar police, as the Chief Minister of the Punjab, to kill his son along with other men in a fake encounter.[31] In the encounter, his two sons and three other people were killed by the police.</p>
+
+			<p class="text-justify">In 2003, an anti-terrorism court summoned Shehbaz and five others accused in alleged extrajudicial killings in 1998. He was in the exile at that time and failed to show-up to the court. Later, the court issued an arrest warrant for Shehbaz. In 2004, Shehbaz attempted to return to Pakistan to appear before the court, but was forcibly deported back to Saudi Arabia.</p>
+
+			<p class="text-justify">In August 2007, the Supreme Court of Pakistan gave its verdict which allowed Sharif brothers to return to Pakistan. In September 2007, an anti-terrorism court in Pakistan ordered police to arrest Shehbaz based on a 2003 arrest warrant. He later got bail from anti-terrorism court Shehbaz denied ordering the alleged killings and said the charges against him were politically motivated. He further said that in 2004 he had landed at the Lahore Airport wanting to appear before the court, but was sent back to Saudi Arabia by the government in violation of the orders of the Supreme Court. In 2008, he was acquitted by the Anti-Terrorism Court in the Sabzazar case.</p>
+
+			<h2>Money Laundering Case</h2>
+			<p>On 28 September 2020, Shehbaz was arrested by the National Accountability Bureau on charges of laundering over ₨7,328 million (US$41 million) in a systematic scheme involving close associates and family members. The Bureau had previously frozen 23 different properties belonging to Sharif, his son, and other family members, claiming that these assets were acquired beyond known sources of income. The NAB alleges that the Shehbaz Sharif family used fake transactions to receive foreign funds, with the transactions being made through a company owned by Sharif's son-in-law, Haroon Yousaf.</p>
+
+			<h2>How did he become a PM? [ Illegally ]</h2>
+			<p class="text-justify">On 10 April 2022, Sharif was nominated as candidate for Prime Minister by opposition parties following a vote of no confidence in incumbent Prime Minister Imran Khan after the 2022 Pakistani constitutional crisis.</p>
+
+			<p class="text-justify">He was elected Prime Minister on 11 April 2022. He took the oath of office on the same day, administered by the Chairman of the Senate, Sadiq Sanjrani, acting for President Arif Alvi, who was on medical leave after complaining of “discomfort”.</p>
+
+			<h2>The Illegal and the most disgraced PM ever</h2>
+			<p>The PML-N with 11 other opposition parties ran a campaign against the Prime Minister Imran Khan (Who's still the most popular leader in Pakistan) to oust him with the help of America. As he visited Russia a day before the war broke out.</p>
 		</div>
 	</div>
 </div>
