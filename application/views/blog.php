b4f64d1eb19c5c93255b2fb931cafe59ce6a26ee
Sayed Saddam Hussain <48250591+sayedsaddam@users.noreply.github.com>
2022-04-21 02:13:31 +0500

    updated blog.php, changed the layout of the page, also updated the form design.

--- application/views/blog.php
+++ application/views/blog.php
@@ -1,12 +1,12 @@
 <div class="container">
 	<div class="row mb-5">
 		<div class="col-md-4">
-			<form action="" method="get">
-				<div class="form-group">
-					<input type="text" class="form-control" name="search" placeholder="Search">
-				</div>
-				<div class="form-group">
-					<button type="submit" class="btn btn-primary btn-sm">Search</button>
+			<form action="#" method="get">
+				<div class="input-group mb-3">
+					<input type="text" class="form-control" placeholder="Search articles..." aria-label="Search articles..." aria-describedby="basic-addon2">
+					<div class="input-group-append">
+						<button class="btn btn-secondary" type="button">Search</button>
+					</div>
 				</div>
 			</form>
 			<h4 class="font-weight-light">Most viewed</h4>
@@ -37,36 +37,48 @@
 				</a>
 			</div>
 		</div>
-		<div class="col-md-7">
+		<div class="col-md-8">
 			<h1 class="font-weight-light">Blog Title</h1>
 			<p>Posted by: <a href="" class="text-primary">Admin</a> on <?= date('l, M jS, Y').' at '.date('H:i:s'); ?></p>
-			<p class="lead">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam ut deserunt illo id omnis nihil ullam inventore laboriosam sequi autem ratione, amet ab, molestias commodi doloribus animi eum sunt soluta.</p>
-			<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magnam non accusantium, harum quos sequi aliquam labore doloribus totam necessitatibus doloremque pariatur repellendus possimus unde dignissimos ducimus sed nihil, quam tempore!</p>
-			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur optio natus nulla obcaecati modi quaerat eum aliquam sint recusandae odit ea perferendis fugiat harum facere, necessitatibus dolore veniam at consequatur.</p>
+			<p class="text-justify lead">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam ut deserunt illo id omnis nihil ullam inventore laboriosam sequi autem ratione, amet ab, molestias commodi doloribus animi eum sunt soluta.</p>
+
+			<p class="text-justify">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magnam non accusantium, harum quos sequi aliquam labore doloribus totam necessitatibus doloremque pariatur repellendus possimus unde dignissimos ducimus sed nihil, quam tempore!</p>
+
+			<p class="text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur optio natus nulla obcaecati modi quaerat eum aliquam sint recusandae odit ea perferendis fugiat harum facere, necessitatibus dolore veniam at consequatur.</p>
+
+			<p class="text-justify">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus unde culpa perferendis, enim quasi quis vel voluptate, sapiente vitae molestiae laborum. Vero tempore nam adipisci earum eveniet molestiae sunt architecto!</p>
+
+			<p class="text-justify">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus unde culpa perferendis, enim quasi quis vel voluptate, sapiente vitae molestiae laborum. Vero tempore nam adipisci earum eveniet molestiae sunt architecto!</p>
 		</div>
 	</div>
 	<div class="row">
-		<div class="col-md-4"></div>
-		<div class="col-md-8">
+		<div class="col-md-12">
 			<h2 class="font-weight-light mb-4" title="Stories you might be interested in...">Recently Published</h2>
 			<div class="row">
-				<div class="col-md-4">
+				<div class="col-md-3">
 					<h4 class="font-weight-light">
 						<a href="">Blog title two</a>
 					</h4>
 					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
 					<a href="">read more...</a>
 				</div>
-				<div class="col-md-4">
+				<div class="col-md-3">
 					<h4 class="font-weight-light">
 						<a href="">Blog title three</a>
 					</h4>
 					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
 					<a href="">read more...</a>
 				</div>
-				<div class="col-md-4">
+				<div class="col-md-3">
 					<h4 class="font-weight-light">
 						<a href="">Blog title four</a>
+					</h4>
+					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+					<a href="">read more...</a>
+				</div>
+				<div class="col-md-3">
+					<h4 class="font-weight-light">
+						<a href="">Blog title five</a>
 					</h4>
 					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
 					<a href="">read more...</a>
