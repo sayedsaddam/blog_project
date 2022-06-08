51c3aeec17d323f31cad65185ca49e5ed94286b8
Sayed Saddam Hussain <48250591+sayedsaddam@users.noreply.github.com>
2022-06-08 17:52:50 +0500

    updated home page, fixed the column layout for small screens.

--- application/views/home.php
+++ application/views/home.php
@@ -52,8 +52,8 @@
 	</div>
 	<!-- proucts list... -->
 	<div class="row mt-3 mb-3">
-		<?php for($i = 1; $i < 9; $i++): ?>
-			<div class="col-md-3 mb-4">
+		<?php for($i = 0; $i < 9; $i++): ?>
+			<div class="col-md-4 col-lg-3 mb-4">
 				<div class="card rounded-0">
 					<div class="card-body">
 						<img src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22843%22%20height%3D%22250%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20843%20250%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_17f6fd83be4%20text%20%7B%20fill%3Argba(255%2C255%2C255%2C.75)%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A42pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_17f6fd83be4%22%3E%3Crect%20width%3D%22843%22%20height%3D%22250%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22314.06640625%22%20y%3D%22144.35%22%3E843x250%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" alt="profile image" class="img-fluid">
