02390e881d4693e041579f754171b0c83eea4c77
Sayed Saddam Hussain <48250591+sayedsaddam@users.noreply.github.com>
2022-07-22 01:00:01 +0500

    update blog_detail.php, printed an 404 message when article with a given slug not found.

--- application/views/blog_detail.php
+++ application/views/blog_detail.php
@@ -1,32 +1,42 @@
 <div class="container">
-	<div class="row">
-		<div class="col-md-4">
-			<h3 class="font-weight-light">Recently Published</h3>
-			<div class="card card-secondary">
-				<div class="card-body">
-					<?php if(!empty($articles)): $counter = 0; foreach($articles as $data): ?>
-						<h3 class="font-weight-light mb-0">
-							<a href="<?= base_url('blog/'.$data->slug); ?>"><?= $data->title; ?></a>
-						</h3>
-						<p class="text-muted mb-2">
-							<small>Posted by: <a href=""><?= $article->fullname; ?></a> on <?= date('D, M jS, Y', strtotime($article->created_at)); ?></small>
-						</p>
-						<p><?= substr($data->blog_description, 0, 100).' ...'; ?></p>
-						<a href="<?= base_url('blog/'.$data->slug); ?>">read more...</a>
-						<hr>
-					<?php endforeach; else: echo 'No articles published yet!'; endif; ?>
+	<?php if($article): ?>
+		<div class="row">
+			<div class="col-md-4">
+				<h3 class="font-weight-light">Recently Published</h3>
+				<div class="card card-secondary">
+					<div class="card-body">
+						<?php if(!empty($articles)): foreach($articles as $data): ?>
+							<h3 class="font-weight-light mb-0">
+								<a href="<?= base_url('blog/'.$data->slug); ?>"><?= $data->title; ?></a>
+							</h3>
+							<p class="text-muted mb-2">
+								<small><?= date('D, M jS, Y', strtotime($article->created_at)); ?></small>
+							</p>
+							<p><?= substr($data->blog_description, 0, 100).' ...'; ?></p>
+							<a href="<?= base_url('blog/'.$data->slug); ?>">read more...</a>
+							<hr>
+						<?php endforeach; else: echo 'No articles published yet!'; endif; ?>
+					</div>
 				</div>
 			</div>
+			<div class="col-md-8">
+				<h1 class="font-weight-light mb-0"><?= $article->title; ?></h1>
+				<p class="text-muted mb-2">
+					<small>Posted by: <a href=""><?= $article->fullname; ?>
+						</a> on <?= date('D, M jS, Y', strtotime($article->created_at)); ?>,
+						Last updated: <?= date('D, M jS, Y', strtotime($article->updated_at)); ?>
+					</small>
+				</p>
+				<p class="text-justify"><?= $article->blog_description; ?></p>
+			</div>
 		</div>
-		<div class="col-md-8">
-			<h1 class="font-weight-light mb-0"><?= $article->title; ?></h1>
-			<p class="text-muted mb-2">
-				<small>Posted by: <a href=""><?= $article->fullname; ?>
-					</a> on <?= date('D, M jS, Y', strtotime($article->created_at)); ?>,
-					Last updated: <?= date('D, M jS, Y', strtotime($article->updated_at)); ?>
-				</small>
-			</p>
-			<p class="text-justify"><?= $article->blog_description; ?></p>
+	<?php else: ?>
+		<div class="row">
+			<div class="col-md-12 text-center pb-4 mb-4 mt-5">
+				<h1 class="font-weight-light mb-0">404<br> Page Not Found!</h1>
+				<p>The Article's either deleted or updated!</p>
+				<a href="<?= base_url('blog'); ?>" class="btn btn-outline-secondary">Back Home</a>
+			</div>
 		</div>
-	</div>	
+	<?php endif; ?>
 </div>
