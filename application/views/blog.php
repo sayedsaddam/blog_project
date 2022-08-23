87f3ca2eb5a1f95ed149736f280dc1782c4ed80e
Sayed Saddam Hussain <48250591+sayedsaddam@users.noreply.github.com>
2022-08-23 09:48:31 +0500

    updated blog.php file, added an error message while no results match the search query.

--- application/views/blog.php
+++ application/views/blog.php
@@ -39,20 +39,20 @@
 				</div>
 				<?php endforeach; elseif(!empty($results)): ?>
 					<?php foreach($results as $data): ?>
-						<div class="col-md-6">
-							<h4 class="font-weight-light mb-0">
-								<a href="<?= base_url('blog/'.$data->slug); ?>"><?= $data->title; ?></a>
-							</h4>
-							<p class="text-muted mb-2">
-								<small>Posted by: <a href=""><?= $data->fullname; ?></a> on <?= date('D, M jS, Y', strtotime($data->created_at)); ?></small>
-							</p>
-							<p>
-								<?= substr($data->blog_description, 0, 200).' &hellip;'; ?>
-							</p>
-							<a href="<?= base_url('blog/'.$data->slug); ?>">read more...</a>
-						</div>
-					<?php endforeach; else: echo 'No articles published.'; endif; ?>
-				</div>
+					<div class="col-md-6">
+						<h4 class="font-weight-light mb-0">
+							<a href="<?= base_url('blog/'.$data->slug); ?>"><?= $data->title; ?></a>
+						</h4>
+						<p class="text-muted mb-2">
+							<small>Posted by: <a href=""><?= $data->fullname; ?></a> on <?= date('D, M jS, Y', strtotime($data->created_at)); ?></small>
+						</p>
+						<p>
+							<?= substr($data->blog_description, 0, 200).' &hellip;'; ?>
+						</p>
+						<a href="<?= base_url('blog/'.$data->slug); ?>">read more...</a>
+					</div>
+				<?php endforeach; else: echo '<div class="col-md-12 mb-5"><div class="alert alert-danger lead">No articles found based on your search query, try another one.</div></div>'; endif; ?>
+			</div>
 			<?php echo $this->pagination->create_links(); ?>
 		</div>
 	</div>
